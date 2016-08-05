860adc2b94813c4621fdb7d5f39eb8591a03a093
Lee Zamparo <zamparo@gmail.com>
2016-08-05 18:31:46 -0400

    fixed trainingRoc.m to read from the .csv file in ../data

--- src/trainingRoc.m
+++ src/trainingRoc.m
@@ -1,18 +1,18 @@
 % Script to reproduce the ROC curve for the foci classifier.
 
-% assumes the following data have been imported: 
-%   SQL_2_380_object.CSV - the object data CSV file output from CP
-%   data - the training set labels and objects in the file Foci_Training_Final_Edited_08_21_09_Removed_Image1.csv
-
 % add libsvm directory to the search path.
 path(path,'./libsvm-mat-3.0-1');
 
-% Extracts the training data.
-[data_set,classes] = extractTraining(data,SQL_2_380_object);
+% Load the training set.
+training_data = dlmread('../data/training_set.csv',',');
+
+% Extract the training data, labels.
+data_set = training_data(:,2:917);
+classes = training_data(:,1);
 
 % Split the training data into a training set and a test set.
-% The random split should product about 80% training, 20% test.
-split_set = 0.75 + randn(size(data,1),1); 
+% The random split should produce about 80% training, 20% test.
+split_set = 0.75 + randn(size(data_set,1),1); 
 split = split_set > 0;
 training_set = data_set(split,:);
 training_classes = classes(split,:);
